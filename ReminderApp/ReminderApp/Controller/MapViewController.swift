f032bd3e117f2f23d6a94631185d8a0e9f76ba77
Mike Conner <connerland@yahoo.com>
2019-07-28 18:06:05 -0400

    started setting up the mapview

--- ReminderApp/ReminderApp/Controller/MapViewController.swift
+++ ReminderApp/ReminderApp/Controller/MapViewController.swift
@@ -7,9 +7,76 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
+    @IBOutlet weak var mapView: MKMapView!
     
+    let locationManager = CLLocationManager()
+    let regionInMeters = 1000.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkLocationServices()
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            // show alert letting the user know they have to turn this on.
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            break
+        case .denied:
+            // show alert instructing them how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestAlwaysAuthorization()
+        case .restricted:
+            // show alert letting them know what's up... (parental control, etc.)
+            break
+        case .authorizedAlways:
+            centerViewOnUserLocation()
+            locationManager.startUpdatingLocation()
+            break
+        @unknown default:
+            print("unknown authorization status")
+        }
+    }
+    
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
     
 }
